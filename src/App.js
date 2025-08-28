f15302abb2320e8325e775215d8341cfa80436b8
Luong Huu Thanh <117010350+fisherman611@users.noreply.github.com>
2025-08-28 16:07:37 +0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ProctoringProvider } from './context/ProctoringContext';
+import ExamRoomDashboard from './pages/ExamRoomDashboard';
+import RoomMonitoring from './pages/RoomMonitoring';
+import LiveProctoring from './pages/LiveProctoring';
+import Header from './components/common/Header';
+import './styles/App.css';
+
+function App() {
+  return (
+    <ProctoringProvider>
+      <Router>
+        <div className="app">
+          <Header />
+          <main className="main-content">
+            <Routes>
+              <Route path="/" element={<ExamRoomDashboard />} />
+              <Route path="/room/:roomId" element={<RoomMonitoring />} />
+              <Route path="/room/:roomId/camera/:cameraId" element={<LiveProctoring />} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </ProctoringProvider>
+  );
+}
+
+export default App;
